7bece48c58d4ce25cef10e88d57bd27d79c9efbb
Omar Osama <o.osama@youngcapital.nl>
2024-02-25 14:28:38 +0100

    Handle bulk actions - Import todos from csv - Download all todos in csv

--- todo.py
+++ todo.py
@@ -1,9 +1,11 @@
-from flask import render_template, request, url_for, redirect
+from flask import render_template, request, url_for, redirect, make_response, flash
 from app import app, db
 from flask_bootstrap import Bootstrap4
 from forms.add_from import AddForm
 from models.todo import Todo
 from datetime import datetime
+import csv
+import io
 
 bootstrap = Bootstrap4(app)
 
@@ -66,3 +68,52 @@ def edit(id):
             db.session.commit()
             return redirect(url_for("index"))
     return render_template('edit.html', todo=todo, form=form)
+
+@app.route("/todos/download")
+def download():
+    data = Todo.query.all()
+
+    csv_data = io.StringIO()
+    csv_writer = csv.writer(csv_data)
+    csv_writer.writerow(Todo.__table__.columns.keys())  # Write headers
+    for row in data:
+        csv_writer.writerow([getattr(row, column.name) for column in Todo.__table__.columns])
+
+    response = make_response(csv_data.getvalue())
+    response.headers['Content-Disposition'] = 'attachment; filename=data.csv'
+    response.headers['Content-type'] = 'text/csv'
+
+    return response
+
+@app.route("/todos/upload", methods=["POST"])
+def upload():
+    file = request.files['file']
+
+    if file.filename == '':
+        flash('You need to provide a csv file')
+        return render_template("index.html", todos=todos)
+
+    csv_data = io.StringIO(file.stream.read().decode('UTF8'), newline=None)
+    csv_reader = csv.reader(csv_data)
+    csv_rows = list(csv_reader)
+
+    header = csv_rows[0][1:]    # ignore 'id'
+    data = csv_rows[1:]
+
+    if len(header) != len(Todo.__table__.columns[1:]):
+        flash('The file provided should have these keys: ' + str(Todo.__table__.columns[1:].keys()))
+        return render_template("index.html", todos=todos)
+
+    for row in data:
+        row_with_id = row[1:]   # ignore 'id'
+        record_data = {header[i]: row_with_id[i] for i in range(len(header)) if header[i] != 'id'}
+        record = Todo(**dict(prepare_row(record_data)))
+        db.session.add(record)
+
+    db.session.commit()
+
+    return redirect(url_for("index"))
+
+def prepare_row(row):
+    row['due_to'] = datetime.strptime(row['due_to'], '%Y-%m-%d')
+    return row
